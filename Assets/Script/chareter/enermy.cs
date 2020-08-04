3859dfec5862135eca06f70df8f0b866024039e4
pjshin3 <tlsvlfwp1@naver.com>
2020-08-05 07:42:17 +0900

    4
    4

--- Assets/Script/chareter/enermy.cs
+++ Assets/Script/chareter/enermy.cs
@@ -8,24 +8,22 @@ public class enermy : MonoBehaviour
     public float speed = 0.5f;
     public Transform me;
     Transform masterLocation;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        me = this.GetComponent<Transform>();
+        masterLocation = GameObject.FindWithTag("Master").GetComponent<Transform>();
+
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-    void Move()
-    {
-        masterLocation = GameObject.Find("Master").transform;
-
-        me.localPosition = new Vector3(masterLocation.localPosition.x * speed * Time.deltaTime, masterLocation.localPosition.y * speed * Time.deltaTime, -1);
+      
     }
 }
