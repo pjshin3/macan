664700842dd3e1e7dc459160d7e76855d7abae91
pjshin3 <tlsvlfwp1@naver.com>
2020-08-06 07:42:16 +0900

    5
    5

--- Assets/Script/chareter/enermy.cs
+++ Assets/Script/chareter/enermy.cs
@@ -14,16 +14,41 @@ public class enermy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        me = this.GetComponent<Transform>();
-        masterLocation = GameObject.FindWithTag("Master").GetComponent<Transform>();
-
-
-       
+        masterLocation = GameObject.Find("Master").transform;  
     }
 
     // Update is called once per frame
     void Update()
     {
       
+    }
+    void FixedUpdate()
+    {
+        moving();
+    }
+
+    void moving()
+    {
+        string dist = "";
+
+        if(masterLocation.position.x < me.position.x)
+        {
+            dist = "Left";
+        }
+        else if(masterLocation.position.x > me.position.x)
+        {
+            dist = "Right";
+        }
+        
+        if(dist == "Left")
+        {
+            me.localScale = new Vector3(1, 1, 1);
+        }else if(dist == "Right")
+        {
+            me.localScale = new Vector3(-1, 1, 1);
+        }
+
+        me.position += masterLocation.position * speed * Time.deltaTime;
+        Debug.Log("지금 마스터의 위치 " + masterLocation.position + masterLocation.localPosition);
     }
 }
